a5a5ffaee48bce65ac21ce51d4fec3b043c6b402
kjd <rlawneo@dreamwiz.com>
2018-11-16 15:19:34 +0900

    1

--- src/main/java/com/ict/erp/controller/LevelInfoController.java
+++ src/main/java/com/ict/erp/controller/LevelInfoController.java
@@ -5,8 +5,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,10 +24,8 @@ public class LevelInfoController {
 	@Autowired
 	private LevelInfoService lis;
 	
-	private static final Logger logger = LoggerFactory.getLogger(LevelInfoController.class);
 	@RequestMapping(value="/",method=RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
