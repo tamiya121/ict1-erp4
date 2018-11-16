4f92f914d8881f834ee280b8b99d4a40b31ff591
kjd <rlawneo@dreamwiz.com>
2018-11-16 15:09:45 +0900

    1

--- src/main/java/com/ict/erp/controller/LevelInfoController.java
+++ src/main/java/com/ict/erp/controller/LevelInfoController.java
@@ -1,9 +1,15 @@
 package com.ict.erp.controller;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ict.erp.HomeController;
 import com.ict.erp.service.LevelInfoService;
 import com.ict.erp.vo.LevelInfo;
 
@@ -20,9 +27,18 @@ public class LevelInfoController {
 	@Autowired
 	private LevelInfoService lis;
 	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	@RequestMapping(value="/",method=RequestMethod.GET)
-	public @ResponseBody String home() {
-		return "home";
+	public @ResponseBody String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		return "home126";
 	}
 	
 	@RequestMapping(value="/linum",method=RequestMethod.GET)
